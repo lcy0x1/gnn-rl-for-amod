2ab649f92b62a6ea4e0aeb422b6b72d31257a510
lcy0x1 <arthurlcy0x1@gmail.com>
2022-11-19 12:01:30 -0800

    tests for imitation

--- src/algos/policy/a2c_training_partial.py
+++ src/algos/policy/a2c_training_partial.py
@@ -31,6 +31,20 @@ class A2CTrainingPrice(A2CBase):
     def configure_optimizers(self):
         actor_params = list(self.actor.parameters())
         critic_params = list(self.critic.parameters())
-        a = ActorOptim(actor_params, lr=3e-4, gamma=0.5, eps=self.eps, device=self.device)
+        a = ActorOptim(actor_params, lr=1e-4, gamma=0.5, eps=self.eps, device=self.device)
         c = CriticOptim(critic_params, lr=1e-3, gamma=0.97, eps=self.eps, device=self.device)
         return a, c
+
+
+class A2CTrainingRebalance(A2CBase):
+
+    def __init__(self, env: AMoD, parser: GNNParser, hidden_size=32, eps=np.finfo(np.float32).eps.item(),
+                 device=torch.device("cpu"), cls: Type[GNNActorBase] = GNNActorVariablePrice, gamma_rate: float = 2000):
+        super().__init__(env, parser, hidden_size, eps, device, cls, gamma_rate)
+
+    def select_action(self, obs):
+        vehicle_vec, price_mat, value = self.forward(obs)
+        vehicle_dist = Dirichlet(vehicle_vec)
+        vehicle_action = vehicle_dist.sample()
+        self.saved_actions.append(SavedAction(vehicle_dist.log_prob(vehicle_action), value))
+        return list(vehicle_action.cpu().numpy()), price_mat.detach().cpu().numpy()
